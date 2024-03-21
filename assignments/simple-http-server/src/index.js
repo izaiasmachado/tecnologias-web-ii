102fda0467a6b6df6701de1bb1a1a85c3c659b32
Izaias Machado <izaiasmachado.dev@gmail.com>
2024-03-21 13:57:34 -0300

    feat: file serving logic

--- assignments/simple-http-server/src/index.js
+++ assignments/simple-http-server/src/index.js
@@ -1,9 +1,45 @@
+const fs = require("fs");
+const path = require("path");
 const http = require("http");
 const port = process.env.PORT || 4000;
 
+const publicFolder = path.join(__dirname, "..", "public");
+
+const getFileContent = (file) => {
+  const filePath = path.join(publicFolder, file);
+
+  if (!fs.existsSync(filePath)) {
+    return null;
+  }
+
+  const fileContent = fs.readFileSync(filePath, "utf-8");
+  return fileContent;
+};
+
+const getPublicFileContent = (file) => {
+  const publicFiles = fs.readdirSync(publicFolder);
+
+  if (!publicFiles.includes(file)) {
+    return null;
+  }
+
+  const fileContent = getFileContent(file);
+  return fileContent;
+};
+
 const server = http.createServer((req, res) => {
-  res.write("<h1>Hello World<h1/>");
-  res.end("<p>Talvez você seja bem vindo</p>");
+  if (req.url === "/") {
+    const fileContent = getFileContent("index.html");
+    return res.end(fileContent);
+  }
+
+  const possibleFileNames = req.url.split("/")[1];
+  const publicFileContent = getPublicFileContent(possibleFileNames);
+  console.log(possibleFileNames);
+  if (publicFileContent) return res.end(publicFileContent);
+
+  res.statusCode = 404;
+  return res.end("404 Not Found");
 });
 
 server.listen(port, () => {
